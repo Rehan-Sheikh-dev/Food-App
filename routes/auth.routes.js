ff177972c980e7d6692316095526dce3ab3ebca4
Rehan Sheikh <rehansheikh0808@gmail.com>
2026-02-02 14:57:02 +0530

    feat: restructure user authentication by creating separate auth controller and middleware

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,6 @@
 import express from "express"
-import { createdUser,loginUser } from "../controllers/user.controller.js";
-import { loginMiddleWare,registerMiddleWare } from "../middlewares/user.middleware.js"
+import { createdUser,loginUser } from "../controllers/auth.controller.js";
+import { loginMiddleWare,registerMiddleWare } from "../middlewares/auth.middleware.js"
 const app = express.Router()
 
 app.post('/register',registerMiddleWare,createdUser)
